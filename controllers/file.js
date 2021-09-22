8ffdfc1d7890fec15fbe5d57e07e8382e4532204
hamzaatalif <hamzaatalif@gmail.com>
2021-09-22 21:02:15 +0500

    restart

--- controllers/file.js
+++ controllers/file.js
@@ -1,4 +1,20 @@
+const multer = require("multer");
+const path = require("path")
+
+
+const storage = multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null,"../uploads/")
+    },
+    filename: (req,file,cb)=>{
+        
+    }
+})
+
 const uploadFile = (req,res,next)=>{
+    if (!req.file) {
+        return res.json({error: "All feilds are required."})
+    }
     res.send("Hello from upload file controller")
     next();
 }
